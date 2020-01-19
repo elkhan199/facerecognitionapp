0054d8ff39cc10f3bcd4e8046f4db4672e7762d6
elkhan199 <ismayilzadeelxan@gmail.com>
2020-01-19 15:09:54 +0900

    Updates

---
+++ precache-manifest.22d36200393ae57efb5dc052abd6491e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8cfd7eaf6a5d5270f437364786aa781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd55e0699f991497997",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "bb3cd14abd051ab432d2",
+    "url": "/static/css/main.2ce2c542.chunk.css"
+  },
+  {
+    "revision": "cdd55e0699f991497997",
+    "url": "/static/js/2.c5917809.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c5917809.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb3cd14abd051ab432d2",
+    "url": "/static/js/main.c5ac6570.chunk.js"
+  },
+  {
+    "revision": "af15c0d4ce7075498753",
+    "url": "/static/js/runtime-main.1ab7c014.js"
+  }
+]);
